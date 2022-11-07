74482f01d3ca29a0fad5f0a9818e10cf994e5b99
AndMJ <andrejoao070@gmail.com>
2022-11-07 20:27:56 +0000

    UPDATE finished first vue page

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const app = Vue.createApp({
     data(){
-        name: "joaquim"
+        return {
+            name: "Joaquim",
+            gender: "male",
+            age:"28",
+            email: "joaquim@email.com",
+            image: "https://randomuser.me/api/portraits/men/11.jpg"
+        }
+    },
+    methods:{
+        async getUser(){
+            const res = await fetch("https://randomuser.me/api")
+            const { results }  = await res.json()
+
+            console.log(results)
+            this.name = results[0].name.first + " " + results[0].name.last
+            this.gender = results[0].gender
+            this.age = results[0].dob.age
+            this.email = results[0].email
+            this.image = results[0].picture.large
+        }
     }
 })
 
-app.mount()
+app.mount("#app")
